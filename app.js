33debe3a8487728d384acfc9f90bc053819610dc
Delhi <delhikrishnan262006@gmail.com>
2025-01-22 15:54:05 +0530

    error fixing

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const admin = require('firebase-admin');
 
 // Get Firebase credentials from environment variables
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,  
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,  
+  project_id: process.env.FIREBASE_PROJECT_ID,  
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,  
 };
 
 admin.initializeApp({
