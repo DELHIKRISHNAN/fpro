7577d07097080d79732e9b8c1cccf8da3d291c04
Delhi <delhikrishnan262006@gmail.com>
2025-02-09 20:34:23 +0530

    Updated project with new changes

--- templates/assets/js/mains.js
+++ templates/assets/js/mains.js
@@ -55,5 +55,17 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+// Toggle Navbar Menu in Mobile View
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show-menu');
+});
+
+// Function to handle logout (example placeholder)
+function logout() {
+  alert('Logged out');
+}
 
 
